69667767d44440d05c6ae3af21d84caa1ef077fa
sujal99099 <101410300@georgebrown.ca>
2025-04-05 16:37:04 -0400

    final code

---
+++ finalShoppersPoint/ShoppersPoint/ShoppersPointTests/ShoppersPointTests.swift
@@ -0,0 +1,16 @@
+// Team Memebers
+
+// Name :- Sujal Sutariya ID:- 101410300
+// Name:- Aditya ID:- 101410341
+// Name :- Leela ID:- 101396586
+
+import Testing
+@testable import ShoppersPoint
+
+struct ShoppersPointTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
